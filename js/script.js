a91ec54249d9c06509da1142cc4b2bc7fb432988
IMI1 <yaroslav.serhiienko@gmail.com>
2016-09-08 22:37:13 +0300

    Изменения

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 
 
-   var link = document.querySelector(".index-info-button");
-   var popup = document.querySelector(".modal-content");
-   var close = document.querySelector(".modal-content-close");
+var link = document.querySelector(".index-info-button");
+var popup = document.querySelector(".modal-content");
+var close = document.querySelector(".modal-content-close");
 
-  link.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.add("modal-content-show");
-  });
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+});
 
-  close.addEventListener("click", function(event) {
-    event.preventDefault();
-    popup.classList.remove("modal-content-show");
-  });
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+});
